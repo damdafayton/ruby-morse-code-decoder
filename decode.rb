1f57e2c9fe9b723c6d6af6aed036b5f80e27a1a6
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-05-06 05:54:07 +0300

    Fix linter errors

--- decode.rb
+++ decode.rb
@@ -1,57 +1,25 @@
 def decode(let)
-  morse_dict = {
-    "a" => ".-",
-    "b" => "-...",
-    "c" => "-.-.",
-    "d" => "-..",
-    "e" => ".",
-    "f" => "..-.",
-    "g" => "--.",
-    "h" => "....",
-    "i" => "..",
-    "j" => ".---",
-    "k" => "-.-",
-    "l" => ".-..",
-    "m" => "--",
-    "n" => "-.",
-    "o" => "---",
-    "p" => ".--.",
-    "q" => "--.-",
-    "r" => ".-.",
-    "s" => "...",
-    "t" => "-",
-    "u" => "..-",
-    "v" => "...-",
-    "w" => ".--",
-    "x" => "-..-",
-    "y" => "-.--",
-    "z" => "--..",
-    " " => " ",
-    "1" => ".----",
-    "2" => "..---",
-    "3" => "...--",
-    "4" => "....-",
-    "5" => ".....",
-    "6" => "-....",
-    "7" => "--...",
-    "8" => "---..",
-    "9" => "----.",
-    "0" => "-----"
-  }
-  reverse_dict={}
+  morse_dict = { 'a' => '.-', 'b' => '-...', 'c' => '-.-.', 'd' => '-..', 'e' => '.', 'f' => '..-.',
+                 'g' => '--.', 'k' => '-.-', 'l' => '.-..', 'm' => '--', 'n' => '-.', 'o' => '---',
+                 'x' => '-..-', 'p' => '.--.', 'q' => '--.-', 'r' => '.-.', 's' => '...', 'w' => '.--',
+                 't' => '-', 'u' => '..-', 'v' => '...-', 'i' => '..', '4' => '....-', 'j' => '.---', '3' => '...--',
+                 'y' => '-.--', 'z' => '--..', ' ' => ' ', '1' => '.----', '0' => '-----', 'h' => '....',
+                 '5' => '.....', '6' => '-....', '7' => '--...', '8' => '---..', '9' => '----.', '2' => '..---' }
+
+  reverse_dict = {}
   morse_dict.each do |key, value|
-    reverse_dict[value]=key
+    reverse_dict[value] = key
   end
-  wordsList = let.split('   ')
-  sentence=''
-  wordsList.each_with_index do |word, idx|
-    lettersList = word.split(' ')
-    lettersList.each do |letter|
+  words_list = let.split('   ')
+  sentence = ''
+  words_list.each_with_index do |word, idx|
+    letters_list = word.split
+    letters_list.each do |letter|
       sentence += reverse_dict[letter]
     end
-    unless idx >= wordsList.length-1
-      sentence += ' ' 
-    end
+    sentence += ' ' unless idx >= words_list.length - 1
   end
-  return sentence
+  sentence
 end
+
+print decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
