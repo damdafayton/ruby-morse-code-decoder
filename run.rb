1264785ef8c4cfa09e15d4f257c42ef58d4b1fc8
backtofayton <yucelolcay@gmail.com>
2022-05-14 01:25:12 +0300

    Update

--- run.rb
+++ run.rb
@@ -2,6 +2,6 @@ require './decode_char.rb'
 require './decode_word.rb'
 require './decode.rb'
 
-p decode_char('.-')
-p decode_word('.- .. -.')
-p decode('.-- .-. .. - .   -- .   -- . ... ... .- --. .')
+p decode_char('.-') # "a"
+p decode_word('.- .. -.') # "ain"
+p decode('.-- .-. .. - .   -- .   -- . ... ... .- --. .') # "write me message"
